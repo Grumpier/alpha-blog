da772a80e4acff8d1969ccc4b3f2723bb32dd6c2
Grumpier <smanus@me.com>
2021-07-12 18:18:28 +0200

    Add more styling, alert messages, flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# stops Rails from creating a field_error class for displaying validation error messages
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
+    html_tag.html_safe
+end
