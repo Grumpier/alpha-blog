1aeaa27dea39a60029305478eddc82c25d994a06
Grumpier <smanus@me.com>
2021-07-15 11:23:45 +0200

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    # helper methods
+    # this is to display an image associated with email address at gravatar website
+    def gravatar_for(user, options = { size: 80 })
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        size = options[:size]
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block" )
+    end
 end
